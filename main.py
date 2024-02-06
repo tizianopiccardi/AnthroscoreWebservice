6cb426c8281f6ea81381c410e813e6de1aecb9b8
Tiziano Piccardi <tiziano.piccardi@epfl.ch>
2024-02-06 11:45:27 -0800

    Added user id to logs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
+import uuid
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_cors import CORS
 import json
 from anthroscore import get_text_score
@@ -32,11 +33,22 @@ def predict():
     else:
         score = "No words found."
 
-    log_entry = json.dumps({"text": text, "entities": entities, "timestamp": datetime.now().isoformat()})
+    user_id = request.cookies.get('user_id')
+
+    response = make_response(score)
+    if user_id is None:
+        user_id = uuid.uuid4().hex
+        response.set_cookie('user_id', user_id)
+
+    log_entry = json.dumps({"text": text,
+                            "entities": entities,
+                            "timestamp": datetime.now().isoformat(),
+                            "user_id": user_id})
+
     with open("requests.log", "a") as log:
         log.write(log_entry + "\n")
 
-    return score
+    return response
 
 
 if __name__ == '__main__':
